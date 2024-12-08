019bbc8227fcb60ee9568169e7661271eedabe56
Daniil Lunev <54506778+DanyaLunev@users.noreply.github.com>
2024-12-08 17:37:30 +0300

    Update DigitalTreeToken.sol

--- DigitalTreeToken.sol
+++ DigitalTreeToken.sol
@@ -1,18 +1,46 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.0;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract DigitalTreeToken is ERC20, Ownable { 
-    constructor(address initialOwner)
-        ERC20("DigitalTreeToken", "DTK")
-        Ownable(initialOwner)
-    {
-        _mint(msg.sender, 100000 * 10 ** decimals());
+contract DigitalTreeToken is ERC20, Ownable {
+    // Общее количество токенов
+    uint256 private constant INITIAL_SUPPLY = 100000 * (10 ** 18);
+
+    constructor(address initialOwner) ERC20("DigitalTreeToken", "DTK") Ownable(initialOwner) {
+        _mint(msg.sender, INITIAL_SUPPLY); // Выпуск токенов на адрес создателя контракта
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+    // Функция для покупки токенов
+    function buyTokens() public payable {
+        require(msg.value > 0, "Send ETH to buy some tokens");
+
+        // Устанавливаем курс токена (например, 1 ETH = 1000 DTK)
+        uint256 tokensToBuy = msg.value * 1000;
+        require(balanceOf(owner()) >= tokensToBuy, "Not enough tokens in the reserve");
+
+        // Переводим токены покупателю
+        _transfer(owner(), msg.sender, tokensToBuy);
+    }
+
+    // Функция для получения ETH за токены
+    function sellTokens(uint256 tokenAmount) public {
+        require(tokenAmount > 0, "Specify an amount of tokens to sell");
+        require(balanceOf(msg.sender) >= tokenAmount, "Not enough tokens");
+
+        // Устанавливаем курс токена (например, 1 ETH = 1000 DTK)
+        uint256 etherAmount = tokenAmount / 1000;
+        require(address(this).balance >= etherAmount, "Not enough ETH in the contract");
+
+        // Переводим токены обратно в контракт
+        _transfer(msg.sender, owner(), tokenAmount);
+        // Переводим ETH продавцу
+        payable(msg.sender).transfer(etherAmount);
+    }
+
+    // Функция для получения баланса контракта
+    function getContractBalance() public view returns (uint256) {
+        return address(this).balance;
     }
 }
